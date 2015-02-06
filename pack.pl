aad2ea676462d05dc9b61604fee89d4409deb9c9
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2015-02-06 23:06:06 +0100

    Generalised download link

--- pack.pl
+++ pack.pl
@@ -3,4 +3,4 @@ title('SWI-Prolog code for "Login with Google"').
 version('0.5.2').
 keywords([http, oauth2, openid, google, login]).
 author('Jan Wielemaker', 'J.Wielemaker@vu.nl').
-download('https://github.com/JanWielemaker/googleclient/archive/V0.5.1.zip').
+download('https://github.com/JanWielemaker/googleclient/archive/V*.zip').
